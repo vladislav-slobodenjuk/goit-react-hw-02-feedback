06c46d784da0e2fa5dc8e0bc43550d6e3e8d8ba7
vladislav-slobodenjuk <vladjun@gmail.com>
2021-12-14 01:07:52 +0200

    stage4

--- src/componenets/Counter/Button.jsx
+++ src/componenets/Counter/Button.jsx
@@ -1,15 +1,15 @@
-            export default function Button({qwe}) {
-              return (
-            <>
-              <button onClick={() => qwe('good')}></button>
-            </>
-              )
-            }
+// export default function Button({qwe}) {
+//   return (
+// <>
+//   <button onClick={() => qwe('good')}></button>
+// </>
+//   )
+// }
 
-            export default function li({qwe}) {
-              return (
-            <>
-              <li qwe={qwe}></li>
-            </>
-              )
-            }
+// export default function li({qwe}) {
+//   return (
+// <>
+//   <li qwe={qwe}></li>
+// </>
+//   )
+// }
